78386e574e93d05801927ddc5007115508f879c9
Leslie <leslie.appl@gmail.com>
2020-12-27 22:19:02 +0800

    finished all

--- File1227/GroupLister.swift
+++ File1227/GroupLister.swift
@@ -9,81 +9,87 @@ import UIKit
 
 class GroupLister: UITableViewController {
 
+    var files = [URL]()
+    var docsurl: URL {
+        do {
+            let fm = FileManager.default
+            return try fm.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: false)
+        } catch {
+            print(error)
+        }
+        return NSURL() as URL // shouldn't happen
+    }
+    
+    let cellID = "Cell"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        
+        let b = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(doAdd))
+        self.navigationItem.rightBarButtonItems = [b]
+        let b2 = UIBarButtonItem(barButtonSystemItem: .refresh, target: self, action: #selector(doRefresh))
+        self.navigationItem.leftBarButtonItems = [b2]
+        self.title = "Groups"
+        self.tableView.register(UITableViewCell.self, forCellReuseIdentifier: self.cellID)
     }
 
-    // MARK: - Table view data source
-
+    
+    @objc func doRefresh (_: Any?) {
+        print("refreshing")
+        do {
+            let fm = FileManager.default
+            self.files = try fm.contentsOfDirectory(at: self.docsurl, includingPropertiesForKeys: nil).filter {
+                    print($0)
+                    if fm.isUbiquitousItem(at:$0) {
+                        print("trying to download \($0)")
+                        try fm.startDownloadingUbiquitousItem(at:$0)
+                    }
+                    return $0.pathExtension == "pplgrp"
+            }
+            self.tableView.reloadData()
+        } catch {
+            print(error)
+        }
+    }
+    
+    @objc func doAdd (_: Any?) {
+        let av = UIAlertController(title: "New Group", message: "Enter name:", preferredStyle: .alert)
+        av.addTextField {$0.autocapitalizationType = .words}
+        av.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        av.addAction(UIAlertAction(title: "OK", style: .default) {
+            _ in
+            guard let name = av.textFields![0].text, !name.isEmpty else {return}
+            let url = self.docsurl.appendingPathComponent((name as NSString).appendingPathExtension("pplgrp")!)
+            // really should check to see if file by this name exists
+            let pl = PeopleLister(fileURL: url)
+            self.navigationController!.pushViewController(pl, animated: true)
+        })
+        self.present(av, animated: true)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        self.doRefresh(nil)
+    }
+    
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        return self.files.count
     }
-
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
+        let cell = tableView.dequeueReusableCell(withIdentifier: self.cellID, for: indexPath)
+        let fileURL = self.files[indexPath.row]
+        cell.textLabel!.text = (fileURL.lastPathComponent as NSString).deletingPathExtension
+        cell.accessoryType = .disclosureIndicator
         return cell
     }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let pl = PeopleLister(fileURL: self.files[indexPath.row])
+        self.navigationController!.pushViewController(pl, animated: true)
     }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
