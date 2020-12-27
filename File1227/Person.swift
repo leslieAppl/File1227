17982a6a6d6a0ceeeb331cd663d987af56bb3dfe
Leslie <leslie.appl@gmail.com>
2020-12-27 15:08:41 +0800

    finished PeopleDocument file

--- File1227/Person.swift
+++ File1227/Person.swift
@@ -5,4 +5,19 @@
 //  Created by leslie on 12/27/20.
 //
 
-import Foundation
+import UIKit
+
+class Person: NSObject, Codable {
+    var firstName: String
+    var lastName: String
+    
+    override var description: String {
+        return self.firstName + " " + self.lastName
+    }
+    
+    init(firstName: String, lastName: String) {
+        self.firstName = firstName
+        self.lastName = lastName
+        super.init()
+    }
+}
